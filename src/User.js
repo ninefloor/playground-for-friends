071ae6d29a61d9ac245ef7f2ca3d14ff0219b923
nine_floor <less0805@gmail.com>
2023-08-08 21:42:04 +0900

    fix: attend에 담기는 요소 변경 및 attend 감지 비주얼화

--- src/User.js
+++ src/User.js
@@ -3,22 +3,30 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 96px;
   height: 120px;
-  background-color: ${(props) => (props.attend ? '#eee' : '#ccc')};
+  background-color: #ccc;
   cursor: pointer;
+  &.active {
+    background-color: #eee;
+  }
 `;
 
 const User = ({ user, attend, setAttend }) => {
-  const { username } = user;
+  const { username, order } = user;
 
   const clickHandler = () => {
     setAttend((prev) => {
-      if (prev.includes(username)) return prev.filter((el) => el !== username);
-      else return [...prev, username];
+      if (prev.map((el) => el.order).includes(order))
+        return prev.filter((el) => el.order !== order);
+      else return [...prev, user];
     });
   };
   return (
     <button onClick={clickHandler}>
-      <Container attend={attend.includes(username)}>{username}</Container>
+      <Container
+        className={attend.map((el) => el.order).includes(order) ? 'active' : ''}
+      >
+        {username}
+      </Container>
     </button>
   );
 };
