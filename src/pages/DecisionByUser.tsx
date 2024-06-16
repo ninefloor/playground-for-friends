d38d749cd4b4958887e3bbc55450ab59c6cd5887
ninefloor <less0805@gmail.com>
2024-06-16 18:00:53 +0900

    add launch check

--- src/pages/DecisionByUser.tsx
+++ src/pages/DecisionByUser.tsx
@@ -1,15 +1,25 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import s from "./DecisionByUser.module.scss";
 import { VoteDecision } from "@components/decisionByUser/VoteDecision";
 import { TierDecision } from "@components/decisionByUser/TierDecision";
+import { useRealtimeDB } from "@utils/useRealtimeDB";
+import { Button } from "@components/atoms/Buttons";
 
 export const DecisionByUser = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [data] = useRealtimeDB("/activeAdmin", false);
 
   switch (location.pathname) {
     case "/vote":
       return (
         <div className={s.container}>
+          {!data?.join && (
+            <div className={s.notLaunch}>
+              <h2>not launch yet</h2>
+              <Button onClick={() => navigate("/")}>back</Button>
+            </div>
+          )}
           <VoteDecision />
         </div>
       );
@@ -17,11 +27,14 @@ export const DecisionByUser = () => {
     case "/tier":
       return (
         <div className={s.container}>
+          {!data?.join && (
+            <div className={s.notLaunch}>
+              <h2>not launch yet</h2>
+              <Button onClick={() => navigate("/")}>back</Button>
+            </div>
+          )}
           <TierDecision />
         </div>
       );
-
-    default:
-      return <div className={s.container}>잘못된 접근입니다.</div>;
   }
 };
