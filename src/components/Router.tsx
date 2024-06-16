7607f0e29bb78eec9f70dfc0674d40b1e31a09ea
ninefloor <less0805@gmail.com>
2024-06-16 17:38:57 +0900

    rollback router refactor

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,7 @@
-import { createRoutes } from "@utils/createRoute";
+import { Admin } from "@pages/Admin";
+import { DecisionByUser } from "@pages/DecisionByUser";
+import { Home } from "@pages/Home";
+import { NotFound } from "@pages/NotFound";
 import userInfo from "@utils/userInfo";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useRecoilValue } from "recoil";
@@ -6,12 +9,64 @@ import { useRecoilValue } from "recoil";
 export const Router = () => {
   const { userId } = useRecoilValue(userInfo);
   const adminId = import.meta.env.VITE_APP_ADMIN_ID;
-
-  const isAdmin = userId === adminId;
-  const isAuthenticated = !!userId;
-
-  const routes = createRoutes(isAdmin, isAuthenticated);
-
-  const router = createBrowserRouter(routes);
-  return <RouterProvider router={router} />;
+  const noAuthRouter = createBrowserRouter([
+    {
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  const adminRouter = createBrowserRouter([
+    {
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/vote",
+          element: <DecisionByUser />,
+        },
+        {
+          path: "/tier",
+          element: <DecisionByUser />,
+        },
+        {
+          path: "/admin",
+          element: <Admin />,
+        },
+      ],
+    },
+  ]);
+  const authRouter = createBrowserRouter([
+    {
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/vote",
+          element: <DecisionByUser />,
+        },
+        {
+          path: "/tier",
+          element: <DecisionByUser />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <RouterProvider
+      router={
+        userId ? (userId === adminId ? adminRouter : authRouter) : noAuthRouter
+      }
+    />
+  );
 };
