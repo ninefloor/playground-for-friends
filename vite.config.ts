677d8e6f852590f0aa771f82feb88dabfe3a8380
ninefloor <less0805@gmail.com>
2025-08-04 20:18:56 +0900

    스타일 및 path alias 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths({ configNames: ["tsconfig.json"] })],
+  resolve: {
+    alias: {
+      "@theme": path.resolve(__dirname, "src/theme.scss"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        additionalData: `@use "@theme" as *;`,
+      },
+    },
+  },
 });
