93abdf63140e17bbdd79d50e81e5eca256ea6196
ninefloor <less0805@gmail.com>
2025-08-08 09:53:15 +0900

    upgrade vite and packages

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,10 @@
-import { MutatingDots } from "react-loader-spinner";
+import loading from "@assets/images/loading.svg";
 import s from "./Loading.module.scss";
 
 export const Loading = () => {
   return (
     <div className={s.container}>
-      <MutatingDots
-        height="100"
-        width="100"
-        color="#ffffff"
-        secondaryColor="#ffffff"
-        radius="20"
-        ariaLabel="mutating-dots-loading"
-        visible={true}
-      />
+      <img src={loading} alt="loading" />
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({ configNames: ["tsconfig.json"] })],
-  resolve: {
-    alias: {
-      "@theme": path.resolve(__dirname, "src/theme.scss"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-    },
-  },
+  plugins: [react(), tsconfigPaths({ projects: ["tsconfig.json"] })],
+  publicDir: "public",
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
         additionalData: `@use "@theme" as *;`,
       },
     },
